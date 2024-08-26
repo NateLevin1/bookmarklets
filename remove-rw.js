c60ac1c87d7775035387a8c597827a286d13b6f9
NateLevin1 <63370104+UltimatePro-Grammer@users.noreply.github.com>
2024-08-26 08:32:54 -0400

    ✨ Add Remove R&W Toolbar bookmarklet

---
+++ remove-rw.js
@@ -0,0 +1,8 @@
+//
+// name: 🪛 Remove R&W Toolbar
+// author: Nate Levin (https://natelev.in)
+// desc: Remove the Read&Write toolbar from the page
+//
+
+document.querySelector("gw-toolbar")?.remove();
+document.querySelector("gw-toolbarear")?.remove();
